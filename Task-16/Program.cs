d1a59d730cae32c71abe714c9453cc7c446f00b6
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-07-13 17:00:46 +0300

    run task16

--- Task-16/Program.cs
+++ Task-16/Program.cs
@@ -7,3 +7,25 @@
 8,9 -> нет
 */
 
+Console.Clear();
+Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int result = numA * numA;
+if (result == numB)
+{
+    Console.WriteLine($"{numA}, {numB} -> да");
+}
+else
+{
+    if (numB * numB == numA)
+    {
+        Console.WriteLine($"{numA}, {numB} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{numA}, {numB} -> нет");
+    }
+}
