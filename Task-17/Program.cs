3f9cf6e94ad821e39e7560b41c3ab625c08ac858
SlavaShaiko <v.shaiko2011@yandex.ru>
2022-07-16 12:29:57 +0300

    run task-18

---
+++ Task-17/Program.cs
@@ -0,0 +1,55 @@
+﻿/*17. Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+номер четверти плоскости, в которой находится эта
+точка.
+Console.Clear();
+Console.WriteLine("Введите координат точек ");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("первая четверть");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("вторая четверть");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("третья четверть");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("четвертая четверть");
+}
+else
+{
+    Console.WriteLine("неверные координаты");
+}
+*/
+
+
+/*17. Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+номер четверти плоскости, в которой находится эта
+точка.
+*/
+Console.Clear();
+Console.WriteLine("Введите координат точек ");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+string Quarter(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return ("первая четверть"); // Решение с помощью метода
+    if (xc < 0 && yc > 0) return ("вторая четверть");
+    if (xc < 0 && yc < 0) return ("третья четверть");
+    if (xc > 0 && yc < 0) return ("четвертая четверть");
+    return ("неверные координаты");
+}
+string quarter = Quarter(x, y);
+Console.WriteLine($"{quarter} {x} : {y}");
+
---
+++ Task-18/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 18. Напишити программу, 
+которая по задонному номеручетверти, 
+показывает диапазон возможных координат точек в этой четверти от x до y
+Console.Clear();
+*/
+Console.WriteLine("Введите номер четверти ");
+int x = Convert.ToInt32(Console.ReadLine());
+string Quarter(int xc)
+{
+    if (xc == 1) return ("xc > 0 и yc > 0");
+    if (xc == 2) return ("xc < 0 и yc > 0");
+    if (xc == 3) return ("xc < 0 и yc < 0");
+    if (xc == 4) return ("xc > 0 и yc < 0");
+    return ("неверные координаты");
+}
+string quarter = Quarter(x);
+Console.WriteLine($"{quarter}");
